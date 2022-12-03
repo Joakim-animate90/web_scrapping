42cad8a9907d22197154e35b4c955b5b441ecf8d
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-12-03 11:54:44 +0300

    scrape posts and add to and array

--- scrape_kenya_law.js
+++ scrape_kenya_law.js
@@ -1,5 +1,8 @@
 import fetch, {FormData} from 'node-fetch';
 
+//import cheerio
+import cheerio from 'cheerio';
+
 // import fs a module for writing files to disk
 import fs from 'fs';
 
@@ -16,7 +19,7 @@ formData.set('date_to', '2022-01-31');
 
 // create a html file without an extension to write the results to
 const file = "kenya_law";
-
+const posts = [];
 // fetch the url with the form data using async and await and the fetch module
 
 const scrape = async () => {
@@ -32,13 +35,26 @@ const scrape = async () => {
             });
             // get the response text
             const html = await response.text();
+            // on response convert date to moment to js
+
             // write the response text to the file and log the response or error
             fs.writeFile(`./public_html/${file}.html`, html, (err) => {
                 if (err) {
                     console.log(err);
                 } else {
+                    const $ = cheerio.load(html);
+                    $('.post').each((index, element) => {
+
+                        //find each post and extract the metadata and provide
+                        const post = $(element).find('p').text().replace(/\s\s+/g, ' ');
+                        posts.push(post);
+
+                    });
+
+                    }
                     console.log('success');
-                }});
+                });
+
         } else {
 
             // on fetching the second page and beyond, the page number is included in the url
@@ -47,6 +63,15 @@ const scrape = async () => {
                 body: formData
             });
             const html = await response.text();
+            const $ = cheerio.load(html);
+            $('.post').each((index, element) => {
+
+                //find each post and extract the metadata and provide
+                const post = $(element).find('p').text().replace(/\s\s+/g, ' ');
+                posts.push(post);
+
+            });
+
             // append the response text to a new html file and log the response or error
             fs.writeFile(`./public_html/${file}${i}.html`, html, (err) => {
                 if (err) {
@@ -56,6 +81,10 @@ const scrape = async () => {
                 }});
             }
         }
+    // log the metadata
+    console.log(posts);
+    // log the number of posts
+    console.log(posts.length);
 
 }
 // call the scrape function and log any errors
