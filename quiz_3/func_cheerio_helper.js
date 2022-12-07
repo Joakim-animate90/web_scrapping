18c244a0859bfcc5fdf94b17624fb4dcce9b18c9
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-12-07 18:02:57 +0300

    Hot fixes

--- quiz_3/func_cheerio_helper.js
+++ quiz_3/func_cheerio_helper.js
@@ -13,48 +13,45 @@ const momentDate = (date) => {
 export default function cheerioHelper(html, posts) {
     const $ = cheerio.load(html);
     $('.post').each((index, element) => {
+        //create an object to hold the post data
+        const post = {};
 
         //find each p and map its key value pairs to an object
-        const post = $(element).find('p').map((index, element) => {
-            // get the text of the p elements and remove the span elements
-
-
-            return {
-                [$(element).find('span').text().replace(':', '')]: $(element).find('span').remove()  ? $(element).text().replace(/(\r\n|\n|\r)/gm, "") : null,
-
-            };
+        $(element).find('p').map((index, element) => {
+            //add key value pairs to the object post
+            post[$(element).find('span').text().replace(':', '')] = $(element).find('span').remove() ? $(element).text().trim() : null;
+            return post;
         }).get();
+
         // find date and convert to moment js and add to object post
-        const date = $(element).find('.date-delivered').map((index, element) => {
+        $(element).find('.date-delivered').map((index, element) => {
             //convert date to moment js to use iso format
-            return {
-                [$(element).find('span').text().replace(':', '')]: momentDate($(element).find('span').remove() ? $(element).text() : null),
-            }
+           post[$(element).find('span').text().replace(':', '')] = momentDate($(element).find('span').remove() ? $(element).text() : null);
+            return post;
         }).get();
         // find the case number and add to object post
-        const caseNumber = $(element).find('.case-number').map((index, element) => {
-            return {
-                [$(element).find('span').text().replace(/(\r\n|\n|\r)/gm, "")]: $(element).text().replace(/(\r\n|\n|\r)/gm, "")
-            };
+        $(element).find('.case-number').map((index, element) => {
+            post[$(element).find('span').text().replace(':', '')] = $(element).find('span').remove() ? $(element).text() : null;
+            return post;
         }).get();
 
 
-        const title = $(element).find('h2').map((index, element) => {
-            return {
-                "title": $(element).text().replace(/(\r\n|\n|\r)/gm, "")
-            };
-        }).get();
-        const link = $(element).find('a').map((index, element) => {
-            return {
-                "link": $(element).attr('href')
-            };
+        $(element).find('h2').map((index, element) => {
+              post['title'] = $(element).text();
+            return post;
+
 
         }).get();
 
+        $(element).find('a').map((index, element) => {
+            post['link'] = $(element).attr('href');
+            return post;
 
-        post.push(date[0], caseNumber[0], title[0], link[0]);
+        }).get();
 
 
+
+        // add the post object to the posts array
         posts.push(post);
 
     });
