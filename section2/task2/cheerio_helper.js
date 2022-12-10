1927a46ebca523e5e27db945e8d1746b9aed3a01
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-12-10 23:55:30 +0300

    Addition of new functionalities

--- section2/task2/cheerio_helper.js
+++ section2/task2/cheerio_helper.js
@@ -1,5 +1,9 @@
 import cheerio from 'cheerio';
 import moment from 'moment';
+import puppeteerHelper from './pupee_teer.js'
+import rename_file from './rename_file.js';
+import fs from "fs";
+import path from "path";
 
 const momentDate = (date) => {
     // convert  03/12/2022' to moment to iso format
@@ -7,8 +11,8 @@ const momentDate = (date) => {
     return momentDate;
 }
 
-export default function cheerioHelper(html) {
-    const records = [];
+export default function cheerioHelper(html, records, page) {
+
     const $ = cheerio.load(html);
     $('.ui-scrollpanel-content').each((index, element) => {
         const record = {};
@@ -24,14 +28,48 @@ export default function cheerioHelper(html) {
                         record['date'] = momentDate($(element).next().find('span').text().trim());
 
                     }
-                    return record[$(element).find('span').text().replace(':', '')] = $(element).next().find('span').text();
+                    return record[$(element).find('span').text().replace(':', " ").trim()] = $(element).next().find('span').text();
                 } else {
                     return;
                 }
-            }).get();
+            });
+            //click page to download pdf file using async and await
+            //use async function
+            async function downloadFile(record, index) {
+                await page.click(`#formularioConsultar\\:dgArchivos\\:0\\:j_idt${148 + 2}`);
+
+            }
+
+            downloadFile(record, index).then(() => {
+                //rename the file using the NR value
+                const dir = "./downloads";
+                //use the value of key NR as the new file name
+                const newFileName = record[0].NR;
+
+
+                //use fs.readdir to read the directory
+                fs.readdir(dir, (err, files) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //use fs.rename to rename the file
+                        fs.rename(path.join(JSON.parse(dir), files[0]), path.join(JSON.parse(dir), JSON.parse(newFileName + '.pdf')), (err) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log('File renamed successfully');
+                            }
+                        });
+                    }
+                });
+            });
+
+
 
             });
-          records.push(record);
+
+          console.log('Timeout 5000s');
+          records.push(record)
           console.log(records);
         });
     }
---
+++ section2/task2/rename_file.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+//create a function to rename the first file in a directory
+
+
+
+export default async function rename_file(record, index) {
+
+    }
+
+
+
