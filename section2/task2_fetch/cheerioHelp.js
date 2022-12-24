9b140e9b74f7c9358b73a3f35662e7061119be85
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-12-24 22:57:11 +0300

    add use of eslint and ensured the first request are working

--- section2/task2_fetch/cheerioHelp.js
+++ section2/task2_fetch/cheerioHelp.js
@@ -1,49 +1,31 @@
-import cheerio from 'cheerio';
-import moment from 'moment';
+// import new cheerio which is not deprecated and use it to load the html
+import cheerio from 'cheerio'
+import moment from 'moment'
 
-
+// eslint-disable-next-line no-unused-vars
 const momentDate = (date) => {
-    // convert  03/12/2022' to moment to iso format
-    const momentDate = moment(date, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    return momentDate;
+  // convert  03/12/2022' to moment to iso format
+  const momentDate = moment(date, 'DD/MM/YYYY').format('YYYY-MM-DD')
+  return momentDate
 }
 
-export default function cheerioHelp(html, records) {
-    console.log('cheerioHelp');
-    const $ = cheerio.load(html);
-    $('.ui-scrollpanel-content').each((index, element) => {
-        const record = {};
+export default function cheerioHelpRemoveTypeHiddenJurisPrudencia (html) {
+  console.log('cheerioHelp')
+  let viewState
+  // Extract the value of the javax.faces.ViewState field using cheerio
+  const $ = cheerio.load(html)
+  // find a class .ui-layout-unit-content ui-widget-content using cheerio
 
-        //find every tr
-        $(element).find('tr').each((index, element) => {
+  $('.ui-layout-unit-content.ui-widget-content').each((index, element) => {
+    // find input field with name javax.faces.ViewState
+    $(element).find('input').each((index, element) => {
+      // remove the input attribute type hidden
+      $(element).removeAttr('type')
 
-            //find every td and map the first td to the key and the second td to the value
-            $(element).find('td').map((index, element) => {
-                if (index === 0) {
-                    //if key is date, convert to moment js
-                    if ($(element).text().trim() === 'Fecha :') {
-                        record['date'] = momentDate($(element).next().find('span').text().trim());
-
-                    }
-                    record[$(element).find('span').text().replace(':', '').trim()] = $(element).next().find('span').text().trim();
-                    // return a json object
-
-                    return record;
-                } else {
-                    return;
-                }
-            });
-            //click page to download pdf file using async and await
-            //use async function
-
-
-        });
-        print('i am hereeeeeeeeeeeeeeeeeeeeee')
-
-        records.push(record)
-        console.log(records);
-    });
+      if ($(element).attr('name') === 'javax.faces.ViewState') {
+        viewState = $(element).attr('value')
+      }
+    })
+  })
+  return viewState
 }
-
-
-
--- section2/task2_fetch/fetchHelper.js
+++ section2/task2_fetch/fetchHelper.js
@@ -1,69 +1,73 @@
-import nodeFetch, {FormData} from 'node-fetch';
-import makeFetchCookie from 'fetch-cookie';
-import jsdom from 'jsdom';
-import cheerioHelp from './cheerioHelp.js';
+import fetch from 'node-fetch'
+import makeFetchCookie from 'fetch-cookie'
+import cheerioHelpRemoveTypeHidden from './cheerioHelp'
 
-
-const {JSDOM} = jsdom;
-const formelementId = 'formularioConsultar';
-const url = 'https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/';
-const fetchCookie = makeFetchCookie(nodeFetch, new makeFetchCookie.toughCookie.CookieJar());
-const formData = new FormData();
-const records = [];
-
-const options = {
-    credentials: 'include',
+const fetchCookie = makeFetchCookie(fetch)
+const url = 'https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/'
+let cookies = ''
+export default async function fetchHelper () {
+  const options = {
+    method: 'GET',
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.3',
-        'Referer': url,
-        'Upgrade-Insecure-Requests': '1',
+
     }
+  }
+
+  const formData = new URLSearchParams()
+
+  await fetchCookie(url, options).then(res => res).then(async body1 => {
+    console.log(body1.status)
+    // check if response involves set-cookie header
+    if (body1.headers.get('set-cookie')) {
+      // Store the new cookie(s) in a variable
+      const newCookies = body1.headers.get('Set-Cookie')
+
+      // update the new cookie(s) to the cookies variable
+      cookies += newCookies
+    }
+
+    const viewState = cheerioHelpRemoveTypeHidden(await body1.text())
+    formData.append('formularioConsultar:corporaciones:2:j_idt142', 'formularioConsultar:corporaciones:2:j_idt142')
+    formData.append('javax.faces.ViewState', viewState)
+    formData.append('j_idt35', 'j_idt35')
+    formData.append('javax.faces.partial.ajax', 'true')
+    formData.append('javax.faces.source', 'formularioConsultar:corporaciones:2:j_idt142')
+    formData.append('javax.faces.partial.execute', 'formularioConsultar:corporaciones:2:j_idt142')
+
+    options.method = 'POST'
+    options.body = formData
+    options.headers.Referer = url
+    options.headers.Cookie = cookies
+
+    console.log(options)
+    return fetchCookie('https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/faces/consulta/SeleccionCorporacion.xhtml', options)
+      .then(res => res).then(async body => {
+        // check if response involves set-cookie header
+        if (body.headers.get('set-cookie')) {
+          // Store the new cookie(s) in a variable
+          const newCookies = body.headers.get('Set-Cookie')
+
+          // update the new cookie(s) to the cookies variable
+          cookies += newCookies
+        }
+        options.method = 'GET'
+        options.body = null
+        options.headers.Cookie = cookies
+        options.headers.Referer = 'https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/faces/consulta/SeleccionCorporacion.xhtml'
+        return fetchCookie('https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/faces/consulta/ConsultaCorporacion.xhtml')
+      }).then(res => res).then(async body => {
+        // check if response involves set-cookie header
+        if (body.headers.get('set-cookie')) {
+          // Store the new cookie(s) in a variable
+          const newCookies = body.headers.get('Set-Cookie')
+
+          // update the new cookie(s) to the cookies variable
+          cookies += newCookies
+        }
+      })
+
+    // await fetchCookie(`https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/faces/consulta/ConsultaCorporacion.xhtml#javax.faces.ViewState=${viewState}`, options)
+    //  .then(res => res).then(async body => console.log(await body.text()));
+  })
 }
-
-
-export default async function fetchHelper() {
-
-
-    await fetchCookie(url, options).then(async (res) => {
-
-        console.log('first fetch done');
-        options.headers['Cookie'] = res.headers.get('set-cookie');
-
-
-        await fetchCookie('https://ratioiurisprudentia.ramajudicial.gov.co/Jurisprudencia/faces/consulta/ConsultaCorporacion.xhtml', options).then(async (res) => {
-            console.log('second fetch done');
-
-            //use jsdom to get the form element
-            const dom = new JSDOM(await res.text(), {url: url});
-            const form = dom.window.document.getElementById(formelementId);
-
-            //remove readonly attribute from inputs
-            form.querySelectorAll('input').forEach((input) => {
-                input.removeAttribute('readonly');
-
-            });
-
-            //set the  formularioConsultar:dtCampos:7:calInicio_input to 03/01/2022
-            formData.append('formularioConsultar:dtCampos:7:calInicio_input', '03/01/2022');
-            //set the formularioConsultar:dtCampos:7:calFin_input to 03/12/2022
-            formData.append('formularioConsultar:dtCampos:7:calFin_input', '04/01/2022');
-            formData.append('formularioConsultar:btBuscarAvanzada', 'Submit');
-            console.log(formData);
-            options.body = formData;
-            options.redirect = 'follow';
-            options.method = 'POST';
-
-            //post the form
-            return fetchCookie(form.action, options).then(async res => await res.text()).then(async body => {
-                console.log('second fetch done');
-
-                cheerioHelp(body, records);
-            });
-        });
-    });
-    console.log(options);
-
-}
-
-fetchHelper();
+fetchHelper()
